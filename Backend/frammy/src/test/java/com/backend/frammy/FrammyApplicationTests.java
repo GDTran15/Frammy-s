4d5f57e0fc17d5c85e0f8628bef6e3bf867d7ec8
GDTran15 <trangiaiduong1512@gmail.com>
2025-09-01 14:51:48 +1000

    config

---
+++ Backend/frammy/src/test/java/com/backend/frammy/FrammyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.frammy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrammyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
