c1fdec7ae6314ab26d481df77877eaef3e9c8788
treeson11 <trisonln@outlook.com>
2025-09-03 15:54:49 +1000

    homepage, navigation between Pages and general setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home';
+import WeeklyResultSummary from  "./Pages/WeeklyResultSummary"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+ export default function App(){
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                  <Route index element = {<Home />} /> //makes default
+                  <Route path = "/home" element = {<Home />} /> //allows naviagation to home by addding the /home to the url
+                  <Route path = "/weekly-result-summary" element = {<WeeklyResultSummary />} />
+                </Routes>
+            </BrowserRouter>
+
+        </div>
+    )
+ }
