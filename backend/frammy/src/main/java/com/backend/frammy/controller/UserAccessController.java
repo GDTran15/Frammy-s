c4936da8324a906870b01bc0993a0390e03265a6
GDTran15 <trangiaiduong1512@gmail.com>
2025-09-22 00:36:40 +1000

    Provide cross origin to make connect to the react

--- backend/frammy/src/main/java/com/backend/frammy/controller/UserAccessController.java
+++ backend/frammy/src/main/java/com/backend/frammy/controller/UserAccessController.java
@@ -7,12 +7,14 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserAccessController {
 
     private final UserService userService;
