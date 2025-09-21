93e37e6733a6acc34b16b4c47a14f12434af0fe7
GDTran15 <trangiaiduong1512@gmail.com>
2025-09-21 16:54:08 +1000

    jwt service with function to create jwttoken and config myuserdetails service

---
+++ backend/frammy/src/main/java/com/backend/frammy/service/MyUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.backend.frammy.service;
+
+import com.backend.frammy.repo.UserRepo;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    final private UserRepo userRepo;
+
+    public MyUserDetailsService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepo.findOptionByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+    }
+}
