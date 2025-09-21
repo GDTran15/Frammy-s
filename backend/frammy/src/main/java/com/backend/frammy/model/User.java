2030b530eb92acf5dbc9a8216084caeb261698d5
GDTran15 <trangiaiduong1512@gmail.com>
2025-09-22 00:36:54 +1000

    import builder

--- backend/frammy/src/main/java/com/backend/frammy/model/User.java
+++ backend/frammy/src/main/java/com/backend/frammy/model/User.java
@@ -2,6 +2,7 @@ package com.backend.frammy.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
