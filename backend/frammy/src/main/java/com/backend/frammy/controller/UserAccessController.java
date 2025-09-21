8488bd022dbb1a8e36aed9a7aa258373733acc12
GDTran15 <trangiaiduong1512@gmail.com>
2025-09-21 22:22:50 +1000

    add @valid and @request body

--- backend/frammy/src/main/java/com/backend/frammy/controller/UserAccessController.java
+++ backend/frammy/src/main/java/com/backend/frammy/controller/UserAccessController.java
@@ -3,10 +3,12 @@ package com.backend.frammy.controller;
 import com.backend.frammy.dto.ApiResponse;
 import com.backend.frammy.dto.RegisterRequestDTO;
 import com.backend.frammy.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +18,7 @@ public class UserAccessController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<ApiResponse<String>> registerAccountForUser(RegisterRequestDTO registerRequestDTO){
+    public ResponseEntity<ApiResponse<String>> registerAccountForUser( @Valid @RequestBody RegisterRequestDTO registerRequestDTO){
             userService.createAccountForUser(registerRequestDTO);
             return ResponseEntity.ok(ApiResponse.success("Register success"));
     }
