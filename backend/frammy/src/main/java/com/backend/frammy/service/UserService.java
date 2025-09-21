30fe322f5018d3c1a76def32d1645380f428ce8e
GDTran15 <trangiaiduong1512@gmail.com>
2025-09-21 22:53:09 +1000

    fix user

--- backend/frammy/src/main/java/com/backend/frammy/service/UserService.java
+++ backend/frammy/src/main/java/com/backend/frammy/service/UserService.java
@@ -1,5 +1,6 @@
 package com.backend.frammy.service;
 
+import com.backend.frammy.model.Role;
 import  com.backend.frammy.model.User;
 import com.backend.frammy.dto.RegisterRequestDTO;
 import com.backend.frammy.exception.UserAlreadyExistException;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +18,7 @@ public class UserService {
     private final UserRepo userRepo;
     private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
 
-
+    @Transactional
     public void createAccountForUser(RegisterRequestDTO registerRequestDTO) {
             boolean userExist = userRepo.existsByUsername(registerRequestDTO.username());
             boolean gmailExist = userRepo.existsByGmail(registerRequestDTO.gmail());
@@ -25,8 +27,9 @@ public class UserService {
             }
             User user = User.builder()
                     .username(registerRequestDTO.username())
-                    .password(bCryptPasswordEncoder.encode(registerRequestDTO.password()))
                     .gmail(registerRequestDTO.gmail())
+                    .password(bCryptPasswordEncoder.encode(registerRequestDTO.password()))
+                    .role(Role.ROLE_USER)
                     .build();
             userRepo.save(user);
     }
