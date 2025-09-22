7d7b3014b5221517d592646c127ff433deedcd74
GDTran15 <trangiaiduong1512@gmail.com>
2025-09-22 11:58:55 +1000

    create login page design without test

--- frontend/src/Pages/LoginPage.js
+++ frontend/src/Pages/LoginPage.js
@@ -1,5 +1,53 @@
+import "../CSS/register.css"
+import NavBar from "../Components/NavBar";
+import FormComponent from "../Components/FormComponent";
+import InputComponent from "../Components/InputComponent";
+import axios from "axios";
+import { useState } from "react";
+
 export default function LoginPage(){
+    const [username,setUsername] = useState("");
+    const [password,setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post({
+            username: username,
+            password : password
+        }).then((res) =>{
+            
+        }
+        
+    )
+    }
+
     return(
-        <h1>Login</h1>
+        <>
+         <NavBar/>
+
+        <FormComponent
+        title="Welcome Back"
+        subTitle="Login into your account"
+        optional="Don't have account? Register"
+        >
+            <form >
+                <InputComponent 
+                labelText="Username"
+                changeHandle={(e) => setUsername(e.target.value)}
+                inputType="text"
+                value={username}
+                placeholderValue="Enter your username"
+                />
+                <InputComponent 
+                labelText="Password"
+                changeHandle={(e) => setPassword(e.target.value)}
+                inputType="password"
+                value={password}
+                placeholderValue="Enter your password"
+                />
+            </form>
+        </FormComponent>
+        
+        </>
     );
 }
