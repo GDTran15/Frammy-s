1ae3bac51c47c3b7148bb82693db63a273eb84c8
GDTran15 <trangiaiduong1512@gmail.com>
2025-09-23 22:26:09 +1000

    demo artist manage feature

--- backend/frammy/src/main/java/com/backend/frammy/repo/ArtistRepo.java
+++ backend/frammy/src/main/java/com/backend/frammy/repo/ArtistRepo.java
@@ -3,6 +3,8 @@ package com.backend.frammy.repo;
 import com.backend.frammy.model.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArtistRepo extends JpaRepository<Artist,Long> {
+import java.util.Optional;
 
+public interface ArtistRepo extends JpaRepository<Artist,Long> {
+    Optional<Artist> findByArtistId(Long artistId);
 }
--- backend/frammy/src/main/java/com/backend/frammy/service/ArtistService.java
+++ backend/frammy/src/main/java/com/backend/frammy/service/ArtistService.java
@@ -2,6 +2,7 @@ package com.backend.frammy.service;
 
 import com.backend.frammy.dto.CreateArtistRequestDTO;
 import com.backend.frammy.dto.ResponseGetArtistDTO;
+import com.backend.frammy.dto.UpdateArtistRequestDTO;
 import com.backend.frammy.mapper.ArtistToDTO;
 import com.backend.frammy.mapper.DtoToArtist;
 import com.backend.frammy.model.Artist;
@@ -9,6 +10,7 @@ import com.backend.frammy.model.Artist;
 import com.backend.frammy.repo.ArtistRepo;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,9 +24,9 @@ public class ArtistService {
     private final ArtistRepo artistRepo;
     private final ArtistToDTO artistToDTO;
 
-    public void createArtist(@Valid CreateArtistRequestDTO createArtistRequestDTO) {
+    public Artist createArtist(@Valid CreateArtistRequestDTO createArtistRequestDTO) {
         Artist newArtist = dtoToArtist.apply(createArtistRequestDTO);
-        artistRepo.save(newArtist);
+        return artistRepo.save(newArtist);
     }
 
     public List<ResponseGetArtistDTO> getAllArtist() {
@@ -32,5 +34,18 @@ public class ArtistService {
                 .stream()
                 .map(artistToDTO)
                 .collect(Collectors.toList());
+    }
+
+    public void updateArtist(Long artistId, @Valid UpdateArtistRequestDTO updateArtistRequestDTO) {
+        Artist artist = artistRepo.findByArtistId(artistId)
+                .orElseThrow(() -> new ObjectNotFoundException("Artist not found", artistId));
+        artist.setArtistName(updateArtistRequestDTO.artistName());
+        artist.setArtistInfo(updateArtistRequestDTO.artistInfo());
+        artist.setAwards(updateArtistRequestDTO.awards());
+        artistRepo.save(artist);
+    }
+
+    public void deleteArtist(Long artistId) {
+        artistRepo.deleteById(artistId);
     }
 }
