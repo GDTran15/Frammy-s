14558e34f4003d2695c3f60f22bb5bc8f7c8c072
GDTran15 <trangiaiduong1512@gmail.com>
2025-09-23 14:32:37 +1000

    fix

--- backend/frammy/src/main/java/com/backend/frammy/service/UserManagementService.java
+++ backend/frammy/src/main/java/com/backend/frammy/service/UserManagementService.java
@@ -3,7 +3,8 @@ package com.backend.frammy.service;
 import com.backend.frammy.dto.CreateUserRequestDTO;
 import com.backend.frammy.mapper.DtoToUser;
 import com.backend.frammy.model.User;
-import com.backend.frammy.repository.UserRepository;
+import com.backend.frammy.repo.UserRepo;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,7 @@ import org.springframework.stereotype.Service;
 public class UserManagementService {
 
     private final DtoToUser dtoToUser;
-    private final UserRepository userRepo;
+    private final UserRepo userRepo;
 
     public void createUser(@Valid CreateUserRequestDTO createUserRequestDTO) {
         User newUser = dtoToUser.apply(createUserRequestDTO);
