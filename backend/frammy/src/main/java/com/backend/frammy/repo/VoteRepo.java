fda8262b0b4a36ad930fb06cc39dd7d4a017a21d
Braeden <braeden.correa@student.uts.edu.au>
2025-09-27 20:51:14 +1000

    voterepo

--- backend/frammy/src/main/java/com/backend/frammy/repo/VoteRepo.java
+++ backend/frammy/src/main/java/com/backend/frammy/repo/VoteRepo.java
@@ -3,11 +3,11 @@ package com.backend.frammy.repo;
 import com.backend.frammy.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface VoteRepo extends JpaRepository<Vote, Long> {
+public interface VoteRepo extends JpaRepository<Vote, Long> {
 
     boolean existsByUser_UserIdAndNominee_NomineeId(Long userId, Long nomineeId);
 
-    long countByUser_User_Id(Long userId);
+    long countByUser_UserId(Long userId);
 
 
 }
