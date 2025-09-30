bf0ac2bbc27a0b8435249e8b2aa406486d85342f
Braeden <braeden.correa@student.uts.edu.au>
2025-09-30 23:21:24 +1000

    voteservice begin
    nearly done just fix up some imports and ensure vote saves to db

--- backend/frammy/src/main/java/com/backend/frammy/service/VoteService.java
+++ backend/frammy/src/main/java/com/backend/frammy/service/VoteService.java
@@ -1,7 +1,10 @@
 package com.backend.frammy.service;
 
 import com.backend.frammy.dto.*;
-import  com.backend.frammy.model.Vote;
+import com.backend.frammy.exception.InvalidInputException;
+import com.backend.frammy.exception.ObjectAlreadyExist;
+import com.backend.frammy.model.Nominee;
+import com.backend.frammy.model.Vote;
 import com.backend.frammy.exception.UserAlreadyExistException;
 import com.backend.frammy.repo.VoteRepo;
 import lombok.RequiredArgsConstructor;
@@ -21,5 +24,41 @@ public class VoteService {
 
     private final VoteRepo voteRepo;
     private final JwtService jwtService;
+
+    @Transactional
+    public void createVote(VoteRequestDTO addVoteRequestDTO, String authHeader){
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            throw new InvalidInputException();
+        }
+
+        String token = authHeader.substring(7);
+
+        Long tokenUserId = jwtService.extractUserId(token);
+        if (tokenUserId == null) {
+            throw new InvalidInputException();
+        }
+
+        if (!tokenUserId.eqals(dto.userId())) {
+            throw new InvalidInputException();
+        }
+
+        Nominee nominee = nomineeRepo.findByNomineeId(dto.nomineeId());
+        if (nominee == null) {
+            throw new InvalidInputException();
+        }
+
+        if (voteRepo.existsByUserIdAndNomineeId(dto.userId(), addVoteRequestDTO.nomineeId())) {
+            throw new ObjectAlreadyExist("You have already voted for this nominee");
+        }
+
+        Vote vote = new Vote();
+        vote.setUserId(addVoteRequestDTO.userId());
+        vote.setNomineeId(addVoteRequestDTO.nomineeId());
+    }
+
+
+
+    if (voteRepo.existsByUserIdAndNomineeId(dto.userId(), dto.nomineeId()))
 
 }
