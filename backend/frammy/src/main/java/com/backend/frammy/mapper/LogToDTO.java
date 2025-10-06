92439f8b7f329731f989b07efb134f3aa56e30c5
GDTran15 <trangiaiduong1512@gmail.com>
2025-10-06 18:11:04 +1100

    fix album update feature

--- backend/frammy/src/main/java/com/backend/frammy/mapper/LogToDTO.java
+++ backend/frammy/src/main/java/com/backend/frammy/mapper/LogToDTO.java
@@ -1,29 +1,29 @@
-package com.backend.frammy.mapper;
-
-import com.backend.frammy.dto.UserLogDTO;
-import com.backend.frammy.model.UserLog;
-
-public class LogToDTO {
-
-    public static UserLogDTO toDTO(UserLog log) {
-        if (log == null) return null;
-
-        return new UserLogDTO(
-                log.getAction(),
-                log.getTarget(),
-                log.getDetails(),
-                log.getTimestamp()
-        );
-    }
-
-    public static UserLog toEntity(UserLogDTO dto) {
-        if (dto == null) return null;
-
-        UserLog log = new UserLog();
-        log.setAction(dto.getAction());
-        log.setTarget(dto.getTarget());
-        log.setDetails(dto.getDetails());
-        log.setTimestamp(dto.getTimestamp());
-        return log;
-    }
-}
+//package com.backend.frammy.mapper;
+//
+//import com.backend.frammy.dto.UserLogDTO;
+//import com.backend.frammy.model.UserLog;
+//
+//public class LogToDTO {
+//
+//    public static UserLogDTO toDTO(UserLog log) {
+//        if (log == null) return null;
+//
+//        return new UserLogDTO(
+//                log.getAction(),
+//                log.getTarget(),
+//                log.getDetails(),
+//                log.getTimestamp()
+//        );
+//    }
+//
+//    public static UserLog toEntity(UserLogDTO dto) {
+//        if (dto == null) return null;
+//
+//        UserLog log = new UserLog();
+//        log.setAction(dto.getAction());
+//        log.setTarget(dto.getTarget());
+//        log.setDetails(dto.getDetails());
+//        log.setTimestamp(dto.getTimestamp());
+//        return log;
+//    }
+//}
